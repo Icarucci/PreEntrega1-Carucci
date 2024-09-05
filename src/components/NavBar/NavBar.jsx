a6a52ca5dc4d216223f636818d010b823da44227
Icarucci <ignaciocarucci@hotmail.com>
2024-09-04 23:19:56 -0300

    Links2

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ export const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
+                <a className="navbar-brand" href="/home">
                     <img src={Logo} alt="Logo" width="300" height="90" className="d-inline-block align-top" />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
